c190976802639f2ac3ca8cb346ed7d980cfec663
DAMMAK26 <iyeddammakpro@gmail.com>
2024-10-27 21:30:27 +0100

    excecution du CNN

--- CNN/costumize_data.py
+++ CNN/costumize_data.py
@@ -15,9 +15,11 @@ class CustomDataset(torch.utils.data.Dataset):
         label = self.dataframe.iloc[idx]['nb_cells']
         # Load the image using OpenCV
         image = cv2.imread(self.folder_image + '/' + img_path, cv2.IMREAD_GRAYSCALE)  # OpenCV loads images in BGR format
+        # Convert the image to PIL format
+        
         if self.transform:
             image = self.transform(image)
-        return image, label
+        return image , label
     def set_path_to_dataset(self, path_from_file_to_dataset):
         self.folder_mask = os.path.join(path_from_file_to_dataset,'dataset/archive/BBBC005_v1_ground_truth/synthetic_2_ground_truth')
         self.folder_image = os.path.join(path_from_file_to_dataset,'dataset/archive/BBBC005_v1_images/BBBC005_v1_images')
